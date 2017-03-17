5cf4fd87921063084db490b1bf3715b1d4c0e426
Travis Thompson <tthompson@renovateamerica.com>
2017-03-17 16:57:38 -0700

    Angular 2 test app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HeroService]
 })
+
 export class AppComponent {
-  title = 'app works!';
+  title = 'Hall of Heros';
+  selectedHero: Hero;
+  heroes: Hero[];
+
+  constructor(private heroService: HeroService) {}
+
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }  
+
+  // select a hero
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+  
 }
